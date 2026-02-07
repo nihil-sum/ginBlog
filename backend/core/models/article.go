9910c5a81bd30ab278102cce46ac75104bf13523
nihil-sum <dylanliu050903@protonmail.com>
2026-02-07 09:04:53 +0800

    new update

--- backend/core/models/article.go
+++ backend/core/models/article.go
@@ -1,1 +1,51 @@
 package models
+
+import "golang.org/x/tools/godoc"
+
+type Article struct {
+	Id       uint   `gorm:"column:id;type:int(11) unsigned;primary_key;AUTO_INCREMENT" json:"id"`
+	Title    string `gorm:"column:title;type:varchar(200);NOT NULL" json:"title"`
+	Status   int    `gorm:"column:status;type:tinyint(1);default:0;NOT NULL" json:"status"`
+	ImageUrl string `gorm:"column:image_url;type:varchar(2000);NOT NULL" json:"imageUrl"`
+	CateId   int    `gorm:"column:cate_id;type:int(11);default:0;NOT NULL" json:"cateId"`
+	IsTop    int    `gorm:"column:is_top;type:tinyint(1);default:0;NOT NULL" json:"isTop"`
+	Views    int    `gorm:"column:views;type:int(11);default:0;NOT NULL" json:"views"`
+	Desc     string `gorm:"column:desc;type:varchar(2000);NOT NULL" json:"desc"`
+	Content  string `gorm:"column:content;type:longtext" json:"content"`
+	Model
+}
+
+func (m *Article) TableNmae() string { return "article" }
+
+type StatRes struct {
+	ArticleCount  int64 `json:articleCount`
+	CategoryCount int64 `json:categoryCount`
+	PageCount     int64 `json:pageCount`
+	TagCount      int64 `json:tagCount`
+}
+
+type ArticleListReq struct {
+	godoc.PageInfo
+	Keyword string `form:"keyword" default:"a"`
+	CateId  int    `form:"cateId" default:"0"`
+}
+type ArticleListRes struct {
+	godoc.PageInfo
+	Data []Article `json:data`
+}
+
+type ArticleInfoReq struct {
+	//
+}
+type ArticleAddReq struct {
+	Title string `json:"title" validate:"required"`
+}
+
+type ArticleUpdateReq struct {
+	Id    uint   `json:"id" validate:"required"`
+	Title string `json:"title" validate:"required"`
+}
+
+type ArticleDelReq struct {
+	Id uint `json:"id" validate:"required"`
+}
--- backend/core/pkg/app/bind.go
+++ backend/core/pkg/app/bind.go
@@ -29,6 +29,66 @@ func BindHeader(c *gin.Context, query interface{}) []string {
 	return Bind("header", c, query)
 }
 
-func Bind() {
-	
+func Bind(flag string, c *gin.Context, data interface{}) []string {
+	var err error
+	switch flag {
+	case "json":
+		err = c.shouldBindJson(data)
+		break
+	case "form":
+		err = c.ShouldBind(data)
+		break
+	case "query":
+		err = c.ShouldBindQuery(data)
+		break
+	case "header":
+		err = c.ShouldBindHeader(data)
+		break
+	}
+	if err != nil {
+		logger.Error(err.Error())
+		return []string{err.Error()}
+	}
+
+	zhTrans := zh.New()
+	enTrans := en.New()
+
+	uni := ut.New(zhTrans, zhTrans, enTrans)
+
+	curLocales := "zh"
+	trans, _ := uni.GetTranslator(curLocales)
+
+	validate := validator.New()
+
+	switch curLocales {
+	case "zh":
+		_ = zh_trans.RegisterDefaultTranslations(validate, trans)
+		_ = validate.RegisterTranslation("unique_customer", trans, func(ut ut.Translator) error {
+			return ut.Add("unique_customer", "用户已存在", false)
+		}, func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T(fe.Tag(), fe.Field())
+			return t
+		})
+	case "en":
+		_ = en_trans.RegisterDefaultTranslations(validate, trans)
+	}
+	validate.RegisterTagNameFunc(func(field reflect.StructField) string {
+		name := strings.SplitN(field.Tag.Get("label"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+
+		return name
+	})
+
+	err = validate.Struct(data)
+	if err != nil {
+		errs := err.(validator.ValidationErrors)
+		var msg []string
+		for _, e := range errs {
+			msg = append(msg, fmt.Sprintf("%s", e.Translate(trans)))
+		}
+		return msg
+	}
+	return []string{}
 }
